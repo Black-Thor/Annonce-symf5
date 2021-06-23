147cb8fb7d73f6f7b0841fbb0256cb08d05e18d2
Stephane <stephane857@live.fr>
2021-06-23 18:00:30 +0200

    modif admin

--- demo/src/Controller/admin/adminAnnoncesController.php
+++ demo/src/Controller/admin/adminAnnoncesController.php
@@ -1,29 +1,43 @@
 <?php 
 namespace  App\Controller\admin;
 
+use App\Entity\NewAnnonces;
+use App\Repository\NewAnnoncesRepository ;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route; 
 
-use App\Repository\NewAnnoncesRepository ; 
-
-
-class LuckyController extends AbstractController
+class adminAnnoncesController extends AbstractController
 {
    
-    private $repository ; 
+    private $repository ;
 
+    /**
+     * adminAnnoncesController constructor.
+     * @param NewAnnoncesRepository $repository
+     */
     public function __construct(NewAnnoncesRepository $repository)
     {
         $this -> repository = $repository ; 
     }
 
     /**
-     * @Route("/path")
+     * @Return Response
+     * @Route("/admin" , name=="admin.annonces.index")
+     *
      */
     public function index(): Response
     {
       $annonces =  $this->repository->findAll(); 
-      return $this->render('admin/index.html.twig', compact('annonces')) ; 
+      return $this->render('admin/index.html.twig', compact('annonces')) ;
+    }
+    /**
+     * @Return Response
+     * @Route("/admin/{id}" , name=="admin.annonces.edit")
+     *
+     */
+    public function edit(NewAnnonces $annonces)
+    {
+        return $this->render('admin/annonces/edit.html.twig', compact('annonces')) ;
     }
 }
